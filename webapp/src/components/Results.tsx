9f86a6d6e5332038b901d843aca5d4959523172d
Leo de Guzman <emmandeg@gmail.com>
2025-02-16 13:50:34 +0800

    feat: make results dynamic for activity one and activity two

--- webapp/src/components/Results.tsx
+++ webapp/src/components/Results.tsx
@@ -16,12 +16,17 @@ const Results = ({ resultsData }: { resultsData: ResultsState }) => {
         {resultsData.rounds.map((round) => (
           <div
             key={round.roundNumber}
-            className="w-full divide-y divide-gray-300"
+            className="w-full divide-y divide-gray-300 "
           >
+            {resultsData.rounds.length > 1 && (
+              <div className="bg-skyblue border-b py-3 font-bold">
+                ROUND {round.roundNumber}
+              </div>
+            )}
             {round.results.map((result) => (
               <div
                 key={result.order}
-                className="flex px-6 justify-between bg-skyblue w-full py-3 cursor-pointer"
+                className="flex px-6 justify-between bg-skyblue w-full py-3"
               >
                 <span>Q{result.order}</span>
                 <span className="text-coralblue font-bold">
